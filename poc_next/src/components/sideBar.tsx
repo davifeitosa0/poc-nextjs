ea16aadcfdc8dc6367f49f5de0eb982158f5e3a6
davifeitosa0 <davi.rodrigues@sptech.school>
2024-08-08 10:54:17 -0300

    FEAT - Criando a estrutura base da side bar

---
+++ poc_next/src/components/sideBar.tsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+  return (
+    <div className="bg-[#073a4b] h-screen w-[6%]">
+      <div className="bg-[#09536b] h-[11%] w-[100%]"></div>
+      <div className="h-[89%] flex items-center justify-center ">
+        <div className="flex flex-col h-[85%] items-center my-auto justify-between">
+          <h1>teste</h1>
+          <h2>teste2</h2>
+          </div>
+      </div>
+    </div>
+  );
+}
