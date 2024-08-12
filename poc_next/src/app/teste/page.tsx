88a87bf942760a685ffb3847954012eb8fc5d9c5
davifeitosa0 <davi.rodrigues@sptech.school>
2024-08-12 10:10:15 -0300

    FEAT - Adicionando o iconify

--- poc_next/src/app/teste/page.tsx
+++ poc_next/src/app/teste/page.tsx
@@ -1,9 +1,11 @@
 import ExemploContador from '@/components/exemploContador';
+import { Icon } from '@iconify/react';
 
 export default function KaorasdasdasiNePage() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <h1> OLá Mundo</h1>
+      <Icon icon="pixelarticons:heart"  style={{color: 'black'}} />
       <ExemploContador />
     </div>
   );
