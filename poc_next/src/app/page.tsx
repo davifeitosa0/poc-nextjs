acaad0bc8921164b50d73e9e6d099d9e6ffd6096
davifeitosa0 <davi.rodrigues@sptech.school>
2024-08-13 12:22:48 -0300

    style: formatando o documento

--- poc_next/src/app/page.tsx
+++ poc_next/src/app/page.tsx
@@ -5,32 +5,30 @@ import PostCardBlog from '@/components/postCard';
 
 export default async function Home() {
   const respota = await fetch(`http://localhost:3000/api/post`);
-  
+
   const resp = await respota.json();
   const posts: Post[] = resp[0];
-console.log(posts)
+  console.log(posts);
   return (
     <main className="min-h-screen bg-[#161616]">
       <div className="flex">
         <SideBar />
         <div className="w-full">
-        <PostCardBlog></PostCardBlog>
-        <div className='px-6'>
-           {posts?.map((post) => (
-            <PostCards
-              key={post.id}
-              id={post.id}
-              nome={post.nome}
-              texto={post.texto}
-              dtPost={post.dtPost}
-              qtd_likes={post.qtd_likes}
-              imagem={post.imagem}
-            />
-          ))}
+          <PostCardBlog></PostCardBlog>
+          <div className="px-6">
+            {posts?.map((post) => (
+              <PostCards
+                key={post.id}
+                id={post.id}
+                nome={post.nome}
+                texto={post.texto}
+                dtPost={post.dtPost}
+                qtd_likes={post.qtd_likes}
+                imagem={post.imagem}
+              />
+            ))}
+          </div>
         </div>
-         
-        </div>
-        
       </div>
     </main>
   );
